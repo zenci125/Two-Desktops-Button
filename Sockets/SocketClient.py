7face57fe8baf346ce5e04782c4689df1e0584d3
evgen1987_rus <isaenko.ea.work@gmail.com>
2023-10-09 05:20:11 +1000

    Fully functional socket server and socket client

--- Sockets/SocketClient.py
+++ Sockets/SocketClient.py
@@ -1,9 +1,15 @@
 import socket # TODO: make it so client opens a socket once and is connected until server is terminated
+import time
 
-host = 'daring.cwi.nl'
-port = 50007
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TODO: this is supposed to be in an infinite loop (unless we exit the desktop app)
+host = socket.gethostname()
+port = 65432
+msgToServer = 'Y' # TODO: this should change depending on what button is pressed
+
 s.connect((host, port))
-s.sendall('N')
+print("Connected with", host)
 
-s.close()
+s.send(msgToServer.encode())
+print("Sent the following message to ", host, ": ", msgToServer, sep="")
+
+# s.close()
